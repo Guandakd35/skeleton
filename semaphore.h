64253f2f2ee65f180a5faf203ef120f60ca253d7
glansbury <glansbury@u.northwestern.edu>
2015-11-22 00:48:12 -0600

    Update semaphore.h

--- semaphore.h
+++ semaphore.h
@@ -2,7 +2,21 @@
 #define _SEMAPHORE_H_
 
 //must create another header file so we can access semphore from outside of the program
-typedef struct sem_t;
+
+//we are creating a sem structure
+typedef struct sem_t {
+
+  int value; //classic value
+  pthread_mutex_t mutex;
+  pthread_cond_t cond;
+
+  //the standby list will have a queue to determine what requests came in first [basically same as threadpool.c]
+  int task_queue_size_limit;
+  int queue_front;
+  int queue_rear;
+  
+} m_sem_t;
+
 
 int sem_wait(m_sem_t *s);
 int sem_post(m_sem_t *s);
