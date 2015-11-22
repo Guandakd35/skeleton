86182b47be160c5a75eb3e7e5ee1956ff4abb099
glansbury <glansbury@u.northwestern.edu>
2015-11-22 05:47:06 -0600

    Update semaphore.c

--- semaphore.c
+++ semaphore.c
@@ -1,14 +1,13 @@
 #include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 #include <pthread.h>
 #include <unistd.h>
 
 #include "semaphore.h"
+#include "seats.h"
 
 #define STANDBY_SIZE 10
-
-int sem_wait(m_sem_t *s);
-int sem_post(m_sem_t *s);
-int sem_check(m_sem_t *s);
 
 
 m_sem_t *sem_init(m_sem_t *st, int value)
@@ -16,6 +15,7 @@ m_sem_t *sem_init(m_sem_t *st, int value)
 
   	 st = (m_sem_t*)malloc(sizeof(m_sem_t)); //we do this in seat.c
 	pthread_mutex_init(&(st->mutex), NULL); //initializing the mutex and condition variables
+	pthread_mutex_init(&(st->mutex1), NULL);
 	pthread_cond_init(&(st->cond), NULL);
 
 	st->value = value; //this is going ot be 10
@@ -24,15 +24,19 @@ m_sem_t *sem_init(m_sem_t *st, int value)
 	st->queue_front = 0;
 	st->queue_rear = 0;
 	st->task_queue_size_limit = STANDBY_SIZE; //can only have 10 threads in standby list at a time
+	st -> usrID = 0;
+	st -> used = 0; //this is to do indicate that 1 thread has left the standby list
 
 	return st;
 }
 
 //will return 1 if there are no longer any spots
-int sem_wait(m_sem_t *s)
+void sem_wait(m_sem_t *s)
 {
-
+	printf("IN semaphore \n");
 	int temp = 0; //All have an index which is unique because threads have unique stacks therefore unique local variables
+	int temp2 = (s-> usrID); //we want the user ID that is calluing the function to be stored on the stack temproarily 
+	//this is the user id for the waiting list
 
 	pthread_mutex_lock(&(s->mutex)); //we have to lock this so other threads using semaphore can't make same changes to state of semaphore
 	
@@ -41,22 +45,37 @@ int sem_wait(m_sem_t *s)
 	
 	--(s->value); //our standby list shrinks by one
 
-	while(s->value <= 10) //Put this thread to sleep until
-	{
-		while((s->queue_front) != temp) //If threads unique ID equals front of queue, then we can leave while loop
+		//printf("your b4temp %d \n", temp);
+		//printf("your b4queue front %d \n", s->queue_front);
+		//printf("your value %d \n", s->value);
+		
+		int firstTime = 1;
+
+		while((s->queue_front != temp) || firstTime)
+		{
+		while (s->used == 0)
 		{
 		pthread_cond_wait(&(s->cond), &(s->mutex)); //all threads will be awaken, but we only want the front thread to run
+		printf("your WHILEtemp %d \n", temp);
+		printf("your WHILEqueue front %d \n", s->queue_front);
+		printf("your WHILEVALUE  %d \n", s->value);
+		firstTime = 0;
 		}
-	}
-	
-	s->queue_front = (s->queue_front + 1) % s->task_queue_size_limit; //increment the queueFront by one to the next one in the Queue
+		}
 
-	pthread_mutex_unlock(&(s->mutex)); 
-	return 1; //we want to return to callin function
+	s->used = 0; //now we need to reset the used value so other threads cannot make the same mistake
+	
+	printf("your AFTERqueue front %d \n", s->queue_front);	
+	printf("Semaphore AWAKE \n");
+	printf("Your USR value in queued being awoken is: %d \n", temp2);	
+	pthread_mutex_unlock(&(s->mutex)); 	
+	s->queue_front = (s->queue_front + 1) % s->task_queue_size_limit; //increment the queueFront by one to the next one in the Queue
+	confirm_seat1(temp2);
 }
 
 int sem_post(m_sem_t *s)
 {
+	printf("inside SEMPOST \n");
 	//TODO
   pthread_mutex_lock(&(s->mutex));
 
@@ -64,6 +83,7 @@ int sem_post(m_sem_t *s)
   {
   ++(s->value);
   }
+  s -> used = 1; //this is to allow one thread to leave the while loop
   pthread_mutex_unlock(&(s->mutex));
   pthread_cond_broadcast(&(s->cond)); //this will wake up all standby threads. 
   //NOTE: ONLY the most recently added one will get executed
@@ -71,17 +91,6 @@ int sem_post(m_sem_t *s)
 }
 
 
-int sem_check(m_sem_t *s)
-{
-	if((s->value) <= 0)
-	{
-		return 1;
-	}
-	else
-	{
-		return 0;
-	}
-}
 //todo - keep track of all occupied and empty seats
 // If all seats are locked and a thread calls sem_wait (only if value>0)
 //impliment the queue +
